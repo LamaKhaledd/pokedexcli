19ca65f6bc846f981db7b0cd0abb91bf08fd40ac
LamaKhaledd <s12027995@stu.najah.edu>
2025-07-22 18:32:16 +0300

    Add help and exit commands to REPL with command registry

--- main.go
+++ main.go
@@ -1,27 +1,63 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands = map[string]cliCommand{}
+
+func init() {
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	}
+
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp(commands),
+	}
+}
+
+
 func main() {
-    scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
-    for {
-        fmt.Print("Pokedex > ")
+	for {
+		fmt.Print("Pokedex > ")
 
-        if !scanner.Scan() {
-            fmt.Println("\nExiting Pokedex.")
-            break
-        }
+		if !scanner.Scan() {
+			fmt.Println("\nExiting Pokedex.")
+			break
+		}
 
-        input := scanner.Text()
-        words := cleanInput(input)
+		input := scanner.Text()
+		words := cleanInput(input)
 
-        if len(words) > 0 {
-            fmt.Printf("Your command was: %s\n", words[0])
-        }
-    }
+		if len(words) == 0 {
+			continue
+		}
+
+		cmdName := words[0]
+		cmd, found := commands[cmdName]
+
+		if !found {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := cmd.callback()
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+		}
+	}
 }
--- repl.go
+++ repl.go
@@ -2,11 +2,32 @@ package main
 
 import (
     "strings"
+		"fmt"
+			"os"
 )
 
 func cleanInput(text string) []string {
-    trimmed := strings.TrimSpace(text)
-    lowered := strings.ToLower(trimmed)
-    words := strings.Fields(lowered)
-    return words
+	trimmed := strings.TrimSpace(text)
+	lowered := strings.ToLower(trimmed)
+	words := strings.Fields(lowered)
+	return words
+}
+
+
+func commandHelp(cmds map[string]cliCommand) func() error {
+	return func() error {
+		fmt.Println("Welcome to the Pokedex!")
+		fmt.Println("Usage:\n")
+		for _, cmd := range cmds {
+			fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+		}
+		fmt.Println()
+		return nil
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil 
 }
