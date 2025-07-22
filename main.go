edfdc1b9b4c30278ee03844225d6595afd2aee2e
LamaKhaledd <s12027995@stu.najah.edu>
2025-07-22 16:05:41 +0300

    Implement basic REPL loop showing first command word

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+    scanner := bufio.NewScanner(os.Stdin)
+
+    for {
+        fmt.Print("Pokedex > ")
+
+        if !scanner.Scan() {
+            fmt.Println("\nExiting Pokedex.")
+            break
+        }
+
+        input := scanner.Text()
+        words := cleanInput(input)
+
+        if len(words) > 0 {
+            fmt.Printf("Your command was: %s\n", words[0])
+        }
+    }
 }
